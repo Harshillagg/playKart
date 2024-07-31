3c4ae5b6387a3e93610f9046f7b67a95b7c2b64b
Harshill Aggarwal <harshillsms@gmail.com>
2024-07-31 17:30:56 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,35 +1,65 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
-import upload from "../middlewares/multer.middleware.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js"
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPasswword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
+import upload from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const userRouter = Router()
+const userRouter = Router();
 
 // register
 userRouter.route("/register").post(
+  //multer middleware before the controller works for temp image uploading in backend server
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
 
-    //multer middleware before the controller works for temp image uploading in backend server
-    upload.fields([  
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    
-    //passing to controller
-    registerUser   
-)
+  //passing to controller
+  registerUser
+);
 
 // login
-userRouter.route("/login").post(loginUser)
+userRouter.route("/login").post(loginUser);
 
 // secured Routes
-userRouter.route("/logout").post(verifyJWT, logoutUser)
+userRouter.route("/logout").post(verifyJWT, logoutUser);
 
-userRouter.route("/refresh-token").post(refreshAccessToken)
+userRouter.route("/refresh-token").post(refreshAccessToken);
 
-export default userRouter
+userRouter.route("/change-password").post(verifyJWT, changeCurrentPasswword);
+
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser);
+
+//patch is used if only one field is needed to update
+userRouter.route("update-account").patch(verifyJWT, updateAccountDetails);
+
+userRouter
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+userRouter
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+userRouter.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+
+userRouter.route("/history").get(verifyJWT, getWatchHistory);
+
+export default userRouter;
